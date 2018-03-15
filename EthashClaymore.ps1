2e03994c758db8d1e8b6f3d2149c3fecc2f1749e
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2018-03-16 00:19:21 +0100

    Add files via upload

---
+++ EthashClaymore.ps1
@@ -0,0 +1,23 @@
+. .\Include.ps1
+
+$Path = ".\\Bin\\Ethash-Claymore\\EthDcrMiner64.exe"
+$Uri = "https://github.com/nemosminer/Claymores-Dual-Ethereum/releases/download/v11.2/Claymore.s.EthereumNVIDIA.GPU.Miner.v11.2.zip"
+$Commands = [PSCustomObject]@{
+    "ethash" = "" #Ethash
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+    [PSCustomObject]@{
+        Type = "NVIDIA"
+        Path = $Path
+        Arguments = " -esm 3 -allpools 1 -allcoins 1 -platform 3 -mport -$($Variables.MinerAPITCPPort) -epool $($Pools.Ethash.Host):$($Pools.Ethash.Port) -ewal $($Pools.Ethash.User) -epsw $($Pools.Ethash.Pass)"
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Live * .99} # substract 1% devfee
+        API = "Claymore"
+        Port = $Variables.MinerAPITCPPort #3333
+        Wrap = $false
+        URI = $Uri
+		User = $Pools.(Get-Algorithm($_)).User
+    }
+}
