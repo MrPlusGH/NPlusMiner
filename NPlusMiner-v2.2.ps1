d5e2ca98d186aef7be6c5a52b09ad14ddcb6898b
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2018-07-02 08:51:28 +0200

    Update NPlusMiner-v2.2.ps1

--- NPlusMiner-v2.2.ps1
+++ NPlusMiner-v2.2.ps1
@@ -290,7 +290,7 @@ $SelGPUCC = $Config.SelGPUCC
 $Variables = [PSCustomObject]@{}
 $MainForm | Add-Member -Name "Variables" -Value $Variables -MemberType NoteProperty -Force
 $Variables | Add-Member -Force @{CurrentProduct = "NPlusMiner"}
-$Variables | Add-Member -Force @{CurrentVersion = [Version]"2.2.2"}
+$Variables | Add-Member -Force @{CurrentVersion = [Version]"2.2.3"}
 $Variables | Add-Member -Force @{StatusText = "Idle"}
 
 $TabControl = New-object System.Windows.Forms.TabControl
