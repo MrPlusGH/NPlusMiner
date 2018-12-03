353853fd5af5e9110fce3a7162ac9c1d549ceec4
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2018-12-03 12:13:23 +0100

    Update ccminerTpruvot.ps1

--- Miners/ccminerTpruvot.ps1
+++ Miners/ccminerTpruvot.ps1
@@ -13,7 +13,7 @@ $Commands = [PSCustomObject]@{
     "sha256t"    = " -d $($Config.SelGPUCC) -i 29" #Sha256t
     "skunk"      = " -d $($Config.SelGPUCC)" #Skunk
     "sonoa"      = " -d $($Config.SelGPUCC)" #Sonoa
-    "timetravel" = " -d $($Config.SelGPUCC)" #Timetravel
+    #"timetravel" = " -d $($Config.SelGPUCC)" #Timetravel
     "tribus"     = " -d $($Config.SelGPUCC)" #Tribus
     #"blake2s" = " -d $($Config.SelGPUCC) --api-remote --api-allow=0/0" #Blake2s
     #"c11"        = " -d $($Config.SelGPUCC)" #C11
