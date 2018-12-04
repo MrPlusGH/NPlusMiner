7e3613b2fabfd6bb91be444dd309e1e3822dd63d
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2018-12-04 10:23:48 +0100

    Add files via upload

--- BrainPlus/zpoolplus/BrainPlus.ps1
+++ BrainPlus/zpoolplus/BrainPlus.ps1
@@ -19,8 +19,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 <#
 Product:        NPlusMiner
 File:           BrainPlus.ps1
-version:        2.2
-version date:   20180408
+version:        4.5.2
+version date:   20181204
 #>
 
 
