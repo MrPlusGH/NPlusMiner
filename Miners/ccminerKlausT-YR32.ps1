4a40acfde500f8e695952ee93927a8738285e09e
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2018-12-04 12:02:19 +0100

    Update ccminerKlausT-YR32.ps1

--- Miners/ccminerKlausT-YR32.ps1
+++ Miners/ccminerKlausT-YR32.ps1
@@ -47,7 +47,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
         Type = "NVIDIA"
         Path = $Path
         Arguments = "--cpu-priority 5 -b $($Variables.NVIDIAMinerAPITCPPort) -N 1 -R 1 -a $_ -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day * .86} # account for 14% stale shares
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week * .86} # account for 14% stale shares
         API = "ccminer"
         Port = $Variables.NVIDIAMinerAPITCPPort
         Wrap = $false
