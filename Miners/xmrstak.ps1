7b09b82d652647de9a26eedd82aeb3386a2dbec9
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2018-12-04 09:04:44 +0100

    2.7.0

--- Miners/xmrstak.ps1
+++ Miners/xmrstak.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
 
 $Path = ".\Bin\CryptoNight-FireIce260\xmr-stak.exe"
-$Uri = "https://github.com/nemosminer/xmr-stak/releases/download/v2.6/xmr-stak-win64-2.6.0.7z"
+$Uri = "https://github.com/fireice-uk/xmr-stak/releases/download/2.7.0/xmr-stak-win64-2.7.0.7z"
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
 $Port = 3335
@@ -10,8 +10,8 @@ $Commands = [PSCustomObject]@{
     "cryptonight_heavy" = "" # CryptoNight-Heavy(cryptodredge faster)
     "cryptonight_lite"  = "" # CryptoNight-Lite
     "cryptonight_v7"    = "" # CryptoNightV7(cryptodredge faster)
-    # "monero"     = "" # Monero(v8)
     "cryptonight_v8"    = "" # CryptoNightV8
+    # "monero"     = "" # Monero(v8)
 }
 
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
@@ -54,7 +54,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
         Type      = "NVIDIA"
         Path      = $Path
         Arguments = "-C $($Pools.$Algorithm_Norm.Name)_$($Algorithm_Norm)_$($Pools.$Algorithm_Norm.User)_Nvidia.txt --noAMD --noCPU -i $($Port)"
-        HashRates = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Name)_$($Algorithm_Norm)_HashRate".Week}
+        HashRates = [PSCustomObject]@{$Algorithm_Norm = $Stats."$($Name)_$($Algorithm_Norm)_HashRate".Week * .98} # substract 2% devfee
         API       = "fireice"
         Port      = $Port
         URI       = $Uri
