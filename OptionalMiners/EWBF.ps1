e0db2be0b101d5f5e337b5ad9bd536378646aa7f
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2018-12-13 09:22:59 +0100

    Update EWBF.ps1

--- OptionalMiners/EWBF.ps1
+++ OptionalMiners/EWBF.ps1
@@ -1,6 +1,6 @@
 . .\Include.ps1
 
-$Path = ".\Bin\NVIDIA-EWBF\\miner.exe"
+$Path = ".\Bin\NVIDIA-EWBF\miner.exe"
 $Uri = "https://github.com/nemosminer/EWBF-Zec-Miner/releases/download/btg/ewbf-btg.7z"
 
 $Commands = [PSCustomObject]@{
