06f00f10977109c384ba25645cfe8a9850f065f3
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2018-12-13 09:19:03 +0100

    Update CryptoDredge.ps1

--- Miners/CryptoDredge.ps1
+++ Miners/CryptoDredge.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
 
 $Path = ".\Bin\NVIDIA-CryptoDredge\CryptoDredge.exe"
-$Uri = "https://github.com/technobyl/CryptoDredge/releases/download/v0.13.0/CryptoDredge_0.13.0_cuda_10.0_windows.zip"
+$Uri = "https://github.com/technobyl/CryptoDredge/releases/download/v0.14.0/CryptoDredge_0.14.0_cuda_10.0_windows.zip"
 
 $Commands = [PSCustomObject]@{
     "allium"            = " -a allium" #Allium (fastetst)
@@ -9,16 +9,18 @@ $Commands = [PSCustomObject]@{
     "bitcore"           = " -a bitcore" #Bitcore
     "blake2s"           = " -a blake2s" #Blake2s
     "c11"               = " -a c11" #C11
-    #"cnv8"              = " -a cnv8" #CryptoNightv8 
-    #"cryptonightheavy"  = " -a cryptonightheavy" # CryptoNightHeavy(fastest)
-    #"cryptonightmonero" = " -a cnv8" # Cryptonightmonero
-    #"cryptonightv7"     = " -a cryptonightv7" # CryptoNightV7(fastest)
+    "cnv8"              = " -a cnv8" #CryptoNightv8 
+    "cryptonightheavy"  = " -a cryptonightheavy" # CryptoNightHeavy(fastest)
+    "cryptonightmonero" = " -a cnv8" # Cryptonightmonero
+    "cryptonightv7"     = " -a cryptonightv7" # CryptoNightV7(fastest)
     "dedal"             = " -a dedal" #Dedal
     "exosis"            = " -a exosis" #Exosis   
     "hmq1725"           = " -a hmq1725" #Hmq1725
     "lbk3"              = " -a lbk3" #Lbk3(test)
     "lyra2v2"           = " -a lyra2v2" #Lyra2RE2 (fastest)
+    "lyra2vc0ban"       = " -a lyra2vc0ban" #Lyra2vc0banHash
     "lyra2z"            = " -a lyra2z" #Lyra2z (fastest)
+    "lyrarev3"          = " -a lyrarev3" #Lyra2REv3 
     "neoscrypt"         = " -a neoscrypt" #NeoScrypt (fastest)
     "phi"               = " -a phi" #Phi
     "phi2"              = " -a phi2" #Phi2 (fastest)
@@ -31,7 +33,6 @@ $Commands = [PSCustomObject]@{
     "x17"               = " -a x17" #X17
     "x21s"              = " -a x21s" #X21s
     "x22i"              = " -a x22i" # X22i
-    #"skein"             = " -a skein" #Skein(slow)
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
@@ -41,12 +42,12 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
         Type      = "NVIDIA"
         Path      = $Path
         Arguments = " --api-type ccminer-tcp --no-color --intensity 6 --cpu-priority 5 --no-crashreport --no-watchdog -r -1 -R 1 -b 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week * .99} # substract 1% devfee
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day * .99} # substract 1% devfee
         API       = "ccminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort
         Wrap      = $false
         URI       = $Uri
-        User      = $Pools.(Get-Algorithm($_)).User
+		User = $Pools.(Get-Algorithm($_)).User
         Host = $Pools.(Get-Algorithm $_).Host
         Coin = $Pools.(Get-Algorithm $_).Coin
     }
