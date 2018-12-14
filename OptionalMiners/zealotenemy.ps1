356b4c0cdc475a10d3b78a7f784041894f166088
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2018-12-14 09:49:55 +0100

    Update zealotenemy.ps1
    1.28

--- OptionalMiners/zealotenemy.ps1
+++ OptionalMiners/zealotenemy.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
 
 $Path = ".\Bin\NVIDIA-zealotenemy\z-enemy.exe"
-$Uri = "https://nemosminer.com/data/optional/z-enemy.1-27-cuda10.0.7z"
+$Uri = "https://nemosminer.com/data/optional/z-enemy.1-28-cuda10.0.7z"
 
 $Commands = [PSCustomObject]@{
     "aeriumx"    = "" #AeriumX(RTX)
