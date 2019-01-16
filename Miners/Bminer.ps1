3d9a400b9be8cdc3af70505f5dbee54814207d1b
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2019-01-16 10:56:56 +0100

    Update Bminer.ps1

--- Miners/Bminer.ps1
+++ Miners/Bminer.ps1
@@ -1,27 +1,30 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
  
-$Path = ".\Bin\NVIDIA-Bminer\bminer.exe"
-$Uri = "https://www.bminercontent.com/releases/bminer-lite-v10.7.0-31ee7bc-CUDA-9.2-amd64.zip"
+$Path = ".\Bin\NVIDIA-Bminer1201\bminer.exe"
+$Uri = "https://www.bminercontent.com/releases/bminer-lite-v12.0.1-eab048c-CUDA-9.2-amd64.zip"
 $Commands = [PSCustomObject]@{
-    #"equihashBTG" = " -uri zhash://" #EquihashBTG(testing)
-    #"equihash" = " -uri stratum://" #Equihash(fastest)
-    #"equihash144" = " -pers auto -uri equihash1445://" #Equihash144(testing)
-    #"zhash" = " -pers auto -uri equihash1445://" #Zhash(testing)
-    #"ethash" = " -uri ethstratum://" #Ethash(testing 2080)
+    #"equihashBTG" = " -uri zhash://" #EquihashBTG(miniZ faster)
+    #"equihash" = " -uri stratum://" #Equihash(Asic)
+    #"equihash144" = " -pers auto -uri equihash1445://" #Equihash144(gminer faster)
+    #"zhash" = " -pers auto -uri equihash1445://" #Zhash(gminer faster)
+    #"ethash" = " -uri ethstratum://" #Ethash(ethminer faster)
+    "aeternity" = " -uri aeternity://" #aeternity(testing)
+    "beam" = " -uri beam://" #beam(testing)
+    "grin" = " -uri cuckaroo29://" #Grin(testing)
 }
 $Port = $Variables.NVIDIAMinerAPITCPPort
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
     [PSCustomObject]@{
-        Type = "NVIDIA"
-        Path = $Path
-        Arguments = " -api 127.0.0.1:$Port -nofee -uri stratum://$($Pools.(Get-Algorithm($_)).User):$($Pools.(Get-Algorithm($_)).Pass.ToString().replace(',','%2C'))@$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week} 
-        API = "Bminer"
-        Port = $Port
-        Wrap = $false
-        URI = $Uri    
+        Type      = "NVIDIA"
+        Path      = $Path
+        Arguments = "$($Commands.$_)$($Pools.(Get-Algorithm($_)).User):$($Pools.(Get-Algorithm($_)).Pass.ToString().replace(',','%2C'))@$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -max-temperature 94 -nofee -devices $($Config.SelGPUCC) -api 127.0.0.1:$Port"
+        HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day}
+        API       = "bminer"
+        Port      = $Port
+        Wrap      = $false
+        URI       = $Uri    
         User = $Pools.(Get-Algorithm($_)).User
         Host = $Pools.(Get-Algorithm $_).Host
         Coin = $Pools.(Get-Algorithm $_).Coin
