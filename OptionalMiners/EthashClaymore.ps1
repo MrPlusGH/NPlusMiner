b2f6685198baab69ad7d692e8a555f414dfdff38
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2019-01-16 11:06:24 +0100

    Update EthashClaymore.ps1

--- OptionalMiners/EthashClaymore.ps1
+++ OptionalMiners/EthashClaymore.ps1
@@ -1,23 +1,23 @@
-if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1;RegisterLoaded(".\Include.ps1")}
+if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
 
 $Path = ".\\Bin\\Ethash-Claymore\\EthDcrMiner64.exe"
-$Uri = "https://github.com/nemosminer/Claymores-Dual-Ethereum/releases/download/v11.9/Claymore.s.Dual.Ethereum.v11.9.cuda.9.1.zip"
+$Uri = "https://github.com/nemosminer/Claymores-Dual-Ethereum/releases/download/v12.0/Claymore.s.Dual.Ethereum.GPU.Miner.v12.0.7z"
 $Commands = [PSCustomObject]@{
-    #"ethash" = " -di $($($Config.SelGPUCC).Replace(',',''))" #Ethash(Ethminer faster)
+    #"ethash" = " -di $($($Config.SelGPUCC).Replace(',',''))" #Ethash(PhoenixMiner_4.0b faster)
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
     [PSCustomObject]@{
-        Type = "NVIDIA"
-        Path = $Path
+        Type      = "NVIDIA"
+        Path      = $Path
         Arguments = "-esm 3 -allpools 1 -allcoins 1 -platform 2 -mode 1 -mport -$($Variables.NVIDIAMinerAPITCPPort) -epool $($Pools.Ethash.Host):$($Pools.Ethash.Port) -ewal $($Pools.Ethash.User) -epsw $($Pools.Ethash.Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Week * .99} # substract 1% devfee
-        API = "claymore"
-        Port = $Variables.NVIDIAMinerAPITCPPort #3333
-        Wrap = $false
-        URI = $Uri
+        API       = "ethminer"
+        Port      = $Variables.NVIDIAMinerAPITCPPort #3333
+        Wrap      = $false
+        URI       = $Uri
         User = $Pools.(Get-Algorithm($_)).User
         Host = $Pools.(Get-Algorithm $_).Host
         Coin = $Pools.(Get-Algorithm $_).Coin
