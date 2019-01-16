c0d106e51b7e4ba16343818984b71d6728dc936c
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2019-01-16 11:08:59 +0100

    Update CryptoDredge.ps1

--- OptionalMiners/CryptoDredge.ps1
+++ OptionalMiners/CryptoDredge.ps1
@@ -1,54 +1,49 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
 
 $Path = ".\Bin\NVIDIA-CryptoDredge\CryptoDredge.exe"
-$Uri = "https://github.com/technobyl/CryptoDredge/releases/download/v0.14.0/CryptoDredge_0.14.0_cuda_10.0_windows.zip"
+$Uri = "https://github.com/technobyl/CryptoDredge/releases/download/v0.16.1/CryptoDredge_0.16.1_cuda_10.0_windows.zip"
 
 $Commands = [PSCustomObject]@{
-    "allium"            = " -a allium" #Allium (fastetst)
-    "bcd"               = " -a bcd" #Bcd
-    "bitcore"           = " -a bitcore" #Bitcore
-    "blake2s"           = " -a blake2s" #Blake2s
-    "c11"               = " -a c11" #C11
-    "cnv8"              = " -a cnv8" #CryptoNightv8 
-    "cryptonightheavy"  = " -a cryptonightheavy" # CryptoNightHeavy(fastest)
-    "cryptonightmonero" = " -a cnv8" # Cryptonightmonero
-    "cryptonightv7"     = " -a cryptonightv7" # CryptoNightV7(fastest)
-    "dedal"             = " -a dedal" #Dedal
-    "exosis"            = " -a exosis" #Exosis   
-    "hmq1725"           = " -a hmq1725" #Hmq1725
-    "lbk3"              = " -a lbk3" #Lbk3(test)
-    "lyra2v2"           = " -a lyra2v2" #Lyra2RE2 (fastest)
-    "lyra2vc0ban"       = " -a lyra2vc0ban" #Lyra2vc0banHash
-    "lyra2v3"           = " -a lyrarev3" #Lyra2v3
-    "lyra2z"            = " -a lyra2z" #Lyra2z (fastest)
-    "lyrarev3"          = " -a lyrarev3" #Lyra2REv3 
-    "neoscrypt"         = " -a neoscrypt" #NeoScrypt (fastest)
-    "phi"               = " -a phi" #Phi
-    "phi2"              = " -a phi2" #Phi2 (fastest)
-    "pipe"              = " -a pipe" #Pipe
-    "polytimos "        = " -a polytimos " #Polytimos 
-    "skunk"             = " -a skunk" #Skunk
-    "tribus"            = " -a tribus" #Tribus (fastest)
-    "x16r"              = " -a x16r" #X16r
-    "x16s"              = " -a x16s" #X16s
-    "x17"               = " -a x17" #X17
-    "x21s"              = " -a x21s" #X21s
-    "x22i"              = " -a x22i" # X22i
+    "allium"            = " --intensity 8 -a allium" #Allium (fastest)
+    "lyra2v3"           = " --intensity 8 -a lyra2v3" #Lyra2v3 (fastest)
+    "lyra2zz "          = " --intensity 8 -a lyra2zz" #Lyra2zz (Testing)
+    "neoscrypt"         = " --intensity 6 -a neoscrypt" #NeoScrypt (fastest)
+    "phi"               = " --intensity 8 -a phi" #Phi (fastest)
+    "phi2"              = " --intensity 8 -a phi2" #Phi2 (fastest)
+    "lyra2vc0ban"       = " --intensity 8 -a lyra2vc0ban" #Lyra2vc0banHash (fastest)
+    "cryptonightheavy"  = " --intensity 8 -a cryptonightheavy" # CryptoNightHeavy(fastest)
+    #"x22i"              = " --intensity 8 -a x22i" # X22i (trex faster)
+    #"tribus"            = " --intensity 8 -a tribus" #Tribus (not profitable)
+    "cnv8"              = " --intensity 8 -a cnv8" #CryptoNightv8 (fastest)
+    "cryptonightmonero" = " --intensity 8 -a cnv8" # Cryptonightmonero (fastest)
+    #"c11"               = " --intensity 8 -a c11" #C11 (trex faster)
+    "skunk"             = " --intensity 8 -a skunk" #Skunk (fastest)
+    "mtp"               = " --intensity 8 -a mtp" #Mtp (not Asic :)
+    #"bcd"               = " --intensity 8 -a bcd" #Bcd (trex faster)
+    "x16rt"             = " --intensity 8 -a x16rt" #X16rt (testing)
+    "x21s"              = " --intensity 8 -a x21s" #X21s (fastest)
+    #"x16s"              = " --intensity 8 -a x16s" #X16s (trex faster)
+    #"x17"               = " --intensity 8 -a x17" #X17 (trex faster)
+    #"bitcore"           = " --intensity 8 -a bitcore" #Bitcore (trex faster)
+    "hmq1725"           = " --intensity 8 -a hmq1725" #Hmq1725 (fastest thanks for the fix)
+    "dedal"             = " --intensity 8 -a dedal" #Dedal (trex faster second place)
+    "pipe"              = " --intensity 8 -a pipe" #Pipe (fastest)
+    #"x16r"              = " --intensity 8 -a x16r" #x16r (trex fastest)
 }
 
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
-$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = " --api-type ccminer-tcp --no-color --intensity 6 --cpu-priority 5 --no-crashreport --no-watchdog -r -1 -R 1 -b 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
+        Arguments = "--no-nvml --api-type ccminer-tcp --no-color --cpu-priority 5 --no-crashreport --no-watchdog -r -1 -R 1 -b 127.0.0.1:$($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://$($Pools.(Get-Algorithm($_)).Host):$($Pools.(Get-Algorithm($_)).Port) -u $($Pools.(Get-Algorithm($_)).User) -p $($Pools.(Get-Algorithm($_)).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{(Get-Algorithm($_)) = $Stats."$($Name)_$(Get-Algorithm($_))_HashRate".Day * .99} # substract 1% devfee
         API       = "ccminer"
         Port      = $Variables.NVIDIAMinerAPITCPPort
         Wrap      = $false
         URI       = $Uri
-		User = $Pools.(Get-Algorithm($_)).User
+        User = $Pools.(Get-Algorithm($_)).User
         Host = $Pools.(Get-Algorithm $_).Host
         Coin = $Pools.(Get-Algorithm $_).Coin
     }
