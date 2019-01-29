ccfeda96b45fb3bacad8aaa80b0f789928814a27
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2019-01-29 17:56:39 +0100

    Update ccminerspmodgit.ps1

--- Miners/ccminerspmodgit.ps1
+++ Miners/ccminerspmodgit.ps1
@@ -4,10 +4,10 @@ $Path = ".\Bin\NVIDIA-ccminerspmodgit\ccminer.exe"
 $Uri = "https://github.com/sp-hash/suprminer/releases/download/spmod-git11/spmodgit11.7z"
 
 $Commands = [PSCustomObject]@{
-    "c11" = " -d $($Config.SelGPUCC)" #C11 (trex faster/ fastest open source)
-    "x16r" = " -d $($Config.SelGPUCC)" #X16r(trex faster/ fastest open source)
-    "x16s" = " -d $($Config.SelGPUCC)" #X16s (trex faster/ fastest open source)
-    "x17" = " -d $($Config.SelGPUCC)" #X17 (trex faster/ fastest open source)
+    #"c11" = " -d $($Config.SelGPUCC)" #C11 (trex faster/ fastest open source)
+    #"x16r" = " -d $($Config.SelGPUCC)" #X16r(trex faster/ fastest open source)
+    #"x16s" = " -d $($Config.SelGPUCC)" #X16s (trex faster/ fastest open source)
+    #"x17" = " -d $($Config.SelGPUCC)" #X17 (trex faster/ fastest open source)
     #"bitcore" = " -d $($Config.SelGPUCC)" #Bitcore
     #"blake2s" = " -d $($Config.SelGPUCC)" #Blake2s
     #"blakecoin" = " -d $($Config.SelGPUCC)" #Blakecoin
