94dd6d9c3264aa371e2594c65558c82fd34aada2
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2019-01-30 10:00:05 +0100

    Update GMiner.ps1

--- OptionalMiners/GMiner.ps1
+++ OptionalMiners/GMiner.ps1
@@ -1,14 +1,14 @@
 if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
  
 $Path = ".\Bin\NVIDIA-Gminer\miner.exe"
-$Uri = "https://nemosminer.com/data/optional/gminer_1_23_minimal_windows64.7z"
+$Uri = "https://nemosminer.com/data/optional/gminer_1_25_minimal_windows64.7z"
 $Commands = [PSCustomObject]@{
     "equihash144"  = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Equihash144 (fastest)
     "zhash"        = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Zhash (fastest)
     "equihash192"  = " --devices $($Config.SelGPUDSTM) --algo 192_7 --pers ZERO_PoW" #Equihash192 (fastest)
     "equihash-btg" = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers BgoldPoW" # Equihash-btg (fastest)
     "equihash96"   = " --devices $($Config.SelGPUDSTM) --algo 96_5 --pers auto" #Equihash96 (fastest)
-    #"beam"         = " --devices $($Config.SelGPUDSTM) --algo 150_5" #Equihash150 (Failing on NH, Devs working on it)
+    "beam"         = " --devices $($Config.SelGPUDSTM) --algo 150_5 --pers Beam-PoW" #Equihash150 (fastest)
 }
 $Port = $Variables.NVIDIAMinerAPITCPPort
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
