9328777765dc0d175f101878120a80590727652c
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2019-02-14 10:38:21 +0100

    Update Bminer.ps1
    NH Pass
    Fix errors in Core log

--- Miners/Bminer.ps1
+++ Miners/Bminer.ps1
@@ -10,18 +10,19 @@ $Commands = [PSCustomObject]@{
     #"ethash" = " -uri ethstratum://" #Ethash(ethminer faster)
     # "aeternity" = " -uri aeternity://" #aeternity(testing)
     # "beam" = " -uri beam://" #beam(testing)
-    "Grincuckaroo29" = " -uri cuckaroo29://" #Grin(Fastest)
-    "grin" = " -uri cuckaroo29://" #Grin(Fastest)
+    "Grincuckaroo29" = " -uri cuckaroo29://" #Grin(testing)
+    "grin" = " -uri cuckaroo29://" #Grin(testing)
 }
 $Port = $Variables.NVIDIAMinerAPITCPPort
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
     $Algo = Get-Algorithm($_)
+    $Pass = If ($Pools.($Algo).Pass -like "*,*") {$Pools.($Algo).Pass.ToString().replace(',','%2C')} else {$Pass}
     [PSCustomObject]@{
         Type      = "NVIDIA"
         Path      = $Path
-        Arguments = "$($Commands.$_)$($Pools.($Algo).User):$($Pools.($Algo).Pass.ToString().replace(',','%2C'))@$($Pools.($Algo).Host):$($Pools.($Algo).Port) -max-temperature 94 -nofee -devices $($Config.SelGPUCC) -api 127.0.0.1:$Port"
+        Arguments = "$($Commands.$_)$($Pools.($Algo).User):$($Pass)@$($Pools.($Algo).Host):$($Pools.($Algo).Port) -max-temperature 94 -nofee -devices $($Config.SelGPUCC) -api 127.0.0.1:$Port"
         HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day}
         API       = "bminer"
         Port      = $Port
