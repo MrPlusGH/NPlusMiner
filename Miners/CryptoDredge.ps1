cd97d659c8d24db025e71845dd7be7891be70ab1
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2019-02-20 18:25:15 +0100

    Update CryptoDredge.ps1
    Exclude NiceHash mtp

--- Miners/CryptoDredge.ps1
+++ Miners/CryptoDredge.ps1
@@ -35,6 +35,7 @@ $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
 $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
     $Algo = Get-Algorithm($_)
+    If ($Algo -eq "mtp" -and $Pools.($Algo).Host -notlike "*nicehash*") {return}
     switch ($_) {
         "mtp" {$Fee = 0.02}
         default {$Fee = 0.01}
