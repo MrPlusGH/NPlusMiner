93f10de965c1d233f89da84463f5ed2c738ae8d9
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2019-03-22 10:27:00 +0100

    Create miniZ.ps1

---
+++ Miners/miniZ.ps1
@@ -0,0 +1,29 @@
+if (!(IsLoaded(".\Include.ps1"))) {. .\Include.ps1; RegisterLoaded(".\Include.ps1")}
+ 
+$Path = ".\Bin\NVIDIA-miniZ\miniZ.exe"
+$Uri = "https://nemosminer.com/data/optional/miniZv1.2lcuda10winx64.7z"
+$Commands = [PSCustomObject]@{
+    "equihash144"  = " --algo 144,5 --pers auto" #Equihash144 (fastest)
+    "zhash"        = " --algo 144,5 --pers auto" #Zhash (fastest)
+    "equihash192"  = " --algo 192,7 --pers auto" #Equihash192 (fastest)
+    "equihash-btg" = " --algo 144,5 --pers BgoldPoW " # Equihash-btg MPH (fastest)
+    "equihash96"   = " --algo 96,5  --pers auto" #Equihash96 (ewbf faster)
+}
+$Port = $Variables.NVIDIAMinerAPITCPPort
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+$Algo = Get-Algorithm($_)
+    [PSCustomObject]@{
+        Type      = "NVIDIA"
+        Path      = $Path
+        Arguments = "--intensity 100 --latency --nocolor --tempunits C -cd $($Config.SelGPUDSTM) --telemetry $($Variables.NVIDIAMinerAPITCPPort) --url $($Pools.($Algo).User)@$($Pools.($Algo).Host):$($Pools.($Algo).Port) --pass $($Pools.($Algo).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Week * .98} # substract 2% devfee
+        API       = "miniZ"
+        Port      = $Variables.NVIDIAMinerAPITCPPort
+        Wrap      = $false
+        URI       = $Uri    
+        User = $Pools.($Algo).User
+        Host = $Pools.($Algo).Host
+        Coin = $Pools.($Algo).Coin
+    }
+}
