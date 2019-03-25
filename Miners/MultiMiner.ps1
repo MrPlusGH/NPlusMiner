0f90622c1af75bf446e33c34e13be9f2910f80df
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2019-03-25 19:36:23 +0100

    Update MultiMiner.ps1

--- Miners/MultiMiner.ps1
+++ Miners/MultiMiner.ps1
@@ -15,7 +15,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
     [PSCustomObject]@{
         Type = "NVIDIA"
         Path = $Path
-        Arguments = " -q -b $($Variables.NVIDIAMinerAPITCPPort) -a $_ -o $($Pools.($Algo).Protocol)://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
+        Arguments = " --max-temp=95 -q -b $($Variables.NVIDIAMinerAPITCPPort) -a $_ -o $($Pools.($Algo).Protocol)://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
         HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Week * 0.99}
         API = "Ccminer"
         Port = $Variables.NVIDIAMinerAPITCPPort
