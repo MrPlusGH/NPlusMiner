baf4eec596b36ccb402fed8f6f457414c72b5eaa
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2019-03-29 17:03:32 +0100

    Update WildRigMulti.ps1
    0.15.4-preview12

--- Miners/WildRigMulti.ps1
+++ Miners/WildRigMulti.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1")}
 
 $Path = ".\Bin\AMD-WildRigMulti\wildrig.exe"
-$Uri = "https://github.com/andru-kun/wildrig-multi/releases/download/0.15.4p11/wildrig-multi-windows-0.15.4-preview11.7z"
+$Uri = "https://github.com/andru-kun/wildrig-multi/releases/download/0.15.4p12/wildrig-multi-windows-0.15.4-preview12.7z"
 
 $Commands = [PSCustomObject]@{
     "bcd"           = " --algo bcd" #BitcoinDiamond
