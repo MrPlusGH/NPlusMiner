d4ef086aa0e2ab96444195de5a765b300fa57343
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2019-04-02 13:36:13 +0200

    Update TTMiner.ps1
    2.2.1

--- OptionalMiners/TTMiner.ps1
+++ OptionalMiners/TTMiner.ps1
@@ -1,11 +1,11 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1")}
 
 $Path = ".\Bin\NVIDIA-TTMiner\TT-Miner.exe"
-$Uri = "https://tradeproject.de/download/Miner/TT-Miner-2.1.17.zip"
-
+$Uri = "https://TradeProject.de/download/Miner/TT-Miner.zip"
+ 
 $Commands = [PSCustomObject]@{
-    "progpow"  = " -a PROGPOW-100 "
-    "mtp"      = " -a MTP-100 " #(Supports NiceHash) 
+    "progpow"  = " -a PROGPOW "
+    "mtp"      = " -a MTP " #(Supports NiceHash) 
 }
  
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
