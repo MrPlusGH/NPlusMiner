a8ba6f6d3a462e83961533fa59350f1acc3e1e27
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2019-06-20 13:03:49 +0200

    BrainPlus 5.0 ZergPool

--- Pools/zergpoolplus.ps1
+++ Pools/zergpoolplus.ps1
@@ -24,26 +24,38 @@ $Request | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty N
     $PoolHost = "$($_)$($HostSuffix)"
     $PoolPort = $Request.$_.port
     $PoolAlgorithm = Get-Algorithm $Request.$_.name
-
+    
     $Divisor = $DivisorMultiplier * [Double]$Request.$_.mbtc_mh_factor
 
 	$Stat = Set-Stat -Name "$($Name)_$($PoolAlgorithm)_Profit" -Value ([Double]$Request.$_.$PriceField / $Divisor * (1 - ($Request.$_.fees / 100)))
 
 	$PwdCurr = if ($PoolConf.PwdCurrency) {$PoolConf.PwdCurrency}else {$Config.Passwordcurrency}
     $WorkerName = If ($PoolConf.WorkerName -like "ID=*") {$PoolConf.WorkerName} else {"ID=$($PoolConf.WorkerName)"}
+    
+    $PoolPassword = If ( ! $Config.PartyWhenAvailable ) {"$($WorkerName),c=$($PwdCurr)"} else { "$($WorkerName),c=$($PwdCurr),m=party.NPlusMiner" }
+    $PoolPassword = If ( $Config.ExcludeSoloCoinsOnBrainPlus -and $Request.$_.MC ) {
+        If ($Config.UseTopCoinOnBrainPlus) {
+            $MC = $Request.$_.MC.Split("/")[0]
+        } else {
+            $MC = $Request.$_.MC
+        }
+        "$($PoolPassword),mc=$($MC)"
+    } else {
+        $PoolPassword
+    }
 	
     if ($PoolConf.Wallet) {
         [PSCustomObject]@{
             Algorithm     = $PoolAlgorithm
-            Info          = ""
-            Price         = $Stat.Live*$PoolConf.PricePenaltyFactor
+            Info          = $MC
+            Price         = $Stat.Live*$PoolConf.PricePenaltyFactor #*$SoloPenalty
             StablePrice   = $Stat.Week
             MarginOfError = $Stat.Week_Fluctuation
             Protocol      = "stratum+tcp"
             Host          = $PoolHost
             Port          = $PoolPort
             User          = $PoolConf.Wallet
-		    Pass          = "$($WorkerName),c=$($PwdCurr)"
+		    Pass          = $PoolPassword
             Location      = $Location
             SSL           = $false
         }
