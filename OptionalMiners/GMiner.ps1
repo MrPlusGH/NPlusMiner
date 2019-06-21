4652dcd160a06fdf84c1a1891c19ade829d77621
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2019-06-21 09:34:44 +0200

    1.46

--- OptionalMiners/GMiner.ps1
+++ OptionalMiners/GMiner.ps1
@@ -1,7 +1,8 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1")}
  
 $Path = ".\Bin\NVIDIA-Gminer\miner.exe"
-$Uri = "https://github.com/develsoftware/GMinerRelease/releases/download/1.36/gminer_1_36_minimal_windows64.zip"
+$Uri = "https://github.com/develsoftware/GMinerRelease/releases/download/1.46/gminer_1_46_windows64.zip"
+
 $Commands = [PSCustomObject]@{
     "equihash144"  = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Equihash144 (fastest)
     "zhash"        = " --devices $($Config.SelGPUDSTM) --algo 144_5 --pers auto" #Zhash (fastest)
@@ -10,6 +11,7 @@ $Commands = [PSCustomObject]@{
     "equihash96"   = " --devices $($Config.SelGPUDSTM) --algo 96_5 --pers auto" #Equihash96 (fastest)
     "beam"         = " --devices $($Config.SelGPUDSTM) --algo 150_5 --pers Beam-PoW" #Equihash150 (fastest)
     "grincuckaroo29"  = " --devices $($Config.SelGPUDSTM) --algo grin29 --pers auto" #Grincuckaroo29 (fastest)
+    "cuckoocycle" = " --devices $($Config.SelGPUDSTM) --algo aeternity --pers auto" #Aeternity
     #"cuckatoo31"  = " --devices $($Config.SelGPUDSTM) --algo grin31 --pers auto" #Cuckatoo31 requires 7.4GB VRam, will work on 8GB cards under Linux and Windows 7, will not work under Windows 10
 }
 $Port = $Variables.NVIDIAMinerAPITCPPort
