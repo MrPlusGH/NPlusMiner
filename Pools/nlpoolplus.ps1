133ebe3ceebb0e988a86574f0794f44e49f2b868
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-09-24 10:14:45 +1200

    correct nlpool estimates
    yescrypt has been changed poolside it is correct now
    equihash 125, 192, 144 and verushash were reporting double what they should of been.

--- Pools/nlpoolplus.ps1
+++ Pools/nlpoolplus.ps1
@@ -27,7 +27,10 @@ $Request | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty N
       $Divisor = 1000000 * [Double]$Request.$_.mbtc_mh_factor
 
     switch ($PoolAlgorithm) {
-        "Yescrypt" {$Divisor *= 100}       #temp fix
+        "equihash125" { $Divisor *= 2 } #temp fix
+        "equihash144" { $Divisor *= 2 } #temp fix
+        "equihash192" { $Divisor *= 2 } #temp fix
+        "verushash"   { $Divisor *= 2 } #temp fix
 
     }
 
