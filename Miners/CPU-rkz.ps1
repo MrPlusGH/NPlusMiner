1d99c6d2aa42251d0f2a2250165477bc684d7208
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2019-10-20 11:41:57 +0200

    CPU-rks v4.2b

---
+++ Miners/CPU-rkz.ps1
@@ -0,0 +1,33 @@
+if (!(IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
+
+$Path = ".\Bin\CPU-RKZ\cpuminer.exe"
+$Uri = "https://github.com/RickillerZ/cpuminer-RKZ/releases/download/V4.2b/cpuminer-RKZ.zip"
+
+$Commands = [PSCustomObject]@{
+    "yespower" = "" #Yespower
+    "yescrypt" = "" #Yescrypt
+    "cpupower" = "" #Cpupower
+    "power2b"  = "" #Power2b 
+}
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+
+    $ThreadCount = $Variables.ProcessorCount - 2
+    
+    $Algo = Get-Algorithm($_)
+    [PSCustomObject]@{
+        Type      = "CPU"
+        Path      = $Path
+        Arguments = "-a $_  --cpu-affinity AAAA -q -t $($ThreadCount) -b $($Variables.CPUMinerAPITCPPort) -o $($Pools.($Algo).Protocol)://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day }
+        API       = "ccminer"
+        Port      = $Variables.CPUMinerAPITCPPort
+        Wrap      = $false
+        URI       = $Uri
+        User      = $Pools.($Algo).User
+        Host      = $Pools.($Algo).Host
+        Coin      = $Pools.($Algo).Coin
+    }
+}
