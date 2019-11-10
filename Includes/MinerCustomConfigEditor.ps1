247327e4d2766384c3cabe35c0d99dd0599366ee
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2019-11-10 09:50:33 +0100

    CPR

--- Includes/MinerCustomConfigEditor.ps1
+++ Includes/MinerCustomConfigEditor.ps1
@@ -1,3 +1,27 @@
+<#
+This file is part of NPlusMiner
+Copyright (c) 2018-2019 MrPlus
+
+NPlusMiner is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+NPlusMiner is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#>
+
+<#
+Product:        NPlusMiner
+File:           MinerCustomConfigEditor.ps1
+version:        5.9.9
+version date:   20191108
+#>
 
 #Rules DGV Click Function 
 function RulesDGVClick(){
