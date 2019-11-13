633d24361390aa6f3898ed1dc7bc20aa7796bead
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2019-11-13 10:47:55 +0100

    Add YescryptR8G

---
+++ Miners/Cpu-rplant.ps1
@@ -0,0 +1,33 @@
+if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1;RegisterLoaded(".\Includes\include.ps1")}
+
+$Path = ".\Bin\CPU-rplant\cpuminer-sse42.exe"
+$Uri = "https://github.com/rplant8/cpuminer-opt-rplant/releases/download/4.0.23/cpuminer-opt-win.zip"
+
+$Commands = [PSCustomObject]@{
+    "yescryptR8G" = "" #YescryptR8
+}
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+
+    switch ($_) {
+        "hodl" {$ThreadCount = $Variables.ProcessorCount}
+        default {$ThreadCount = $Variables.ProcessorCount - 2}
+    }
+
+	$Algo = Get-Algorithm($_)
+    [PSCustomObject]@{
+        Type = "CPU"
+        Path = $Path
+        Arguments = "--cpu-affinity AAAA -q -t $($ThreadCount) -b $($Variables.CPUMinerAPITCPPort) -a $_ -o $($Pools.($Algo).Protocol)://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
+        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Week}
+        API = "Ccminer"
+        Port = $Variables.CPUMinerAPITCPPort
+        Wrap = $false
+        URI = $Uri
+        User = $Pools.($Algo).User
+        Host = $Pools.($Algo).Host
+        Coin = $Pools.($Algo).Coin
+    }
+}
