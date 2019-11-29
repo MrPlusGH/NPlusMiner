8fe473ee5505f68d8851fda8d556b546aacc6c5b
NemosMiner <50767117+Minerx117@users.noreply.github.com>
2019-11-30 09:12:15 +1300

    add ccminer lyra2z330
    currently most profitable algo for 1070/1660ti's gpu's recommend a high memory ) OC +500-+750 is optimal

---
+++ Miners/ccminerlyra2z330.ps1
@@ -0,0 +1,37 @@
+if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1")}
+
+$Path = ".\Bin\NVIDIA-ccminerlyra2z330v3\ccminer.exe"
+$Uri = "https://github.com/Minerx117/ccminer8.21r9-lyra2z330/releases/download/v3/ccminerlyra2z330v3.zip"
+ 
+$Commands = [PSCustomObject]@{
+    "lyra2z330" = "" #lyra2z330
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+	$Algo =$_
+	$AlgoNorm = Get-Algorithm($_)
+
+    $Pools.($AlgoNorm) | foreach {
+        $Pool = $_
+        invoke-Expression -command ( $MinerCustomConfigCode )
+        If ($AbortCurrentPool) {Return}
+
+        $Arguments = "-b $($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -o stratum+tcp://$($Pool.Host):$($Pool.Port) -a lyra2z330 -i 12.5 -u $($Pool.User) -p $($Password)"
+
+        [PSCustomObject]@{
+            Type      = "NVIDIA"
+            Path      = $Path
+            Arguments = Merge-Command -Slave $Arguments -Master $CustomCmdAdds -Type "Command"
+            HashRates = [PSCustomObject]@{($AlgoNorm) = $Stats."$($Name)_$($AlgoNorm)_HashRate".Day}
+            API       = "ccminer"
+            Port      = $Variables.NVIDIAMinerAPITCPPort #4068
+            Wrap      = $false
+            URI       = $Uri
+            User      = $Pool.User
+            Host      = $Pool.Host
+            Coin      = $Pool.Coin
+        }
+    }
+}
