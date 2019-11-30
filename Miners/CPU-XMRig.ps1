f7a700e10d212515fcedbf1415f20837655f8640
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2019-11-30 10:24:56 +0100

    Add RandomX (NiceHash)

--- Miners/CPU-XMRig.ps1
+++ Miners/CPU-XMRig.ps1
@@ -1,21 +1,20 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1;RegisterLoaded(".\Includes\include.ps1")}
 
 $Path = ".\Bin\CPU-XMRig\xmrig.exe"
-$Uri = "https://github.com/xmrig/xmrig/releases/download/v2.6.2/xmrig-2.6.2-msvc-win64.zip"
+$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/v5.0.1/xmrig-5.0.1-msvc-cuda10_1-win64.7z"
 
 $Commands = [PSCustomObject]@{
-    #"cryptonight" = "" #Cryptonight
-    #"cryptonight-lite" = "" #Cryptonight-lite
-    #"cryptonight-heavy" = "" #Cryptonight-Heavy
-    #"cryptonightV7" = "" #CryptonightV7
+    # "cryptonightr"        = " -a cryptonight/r --nicehash" #cryptonight/r
+    # "cryptonight-monero"  = " -a cryptonight/r" #cryptonight/r
+    "randomxmonero"       = " -a rx/0 --nicehash" #RandomX
 }
-
+ 
 $ThreadCount = $ThreadCount = $Variables.ProcessorCount - 2
 
 $Port = $Variables.CPUMinerAPITCPPort #2222
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
 
-$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | ForEach-Object {
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | % { 
     $Algo =$_
 	$AlgoNorm = Get-Algorithm($_)
 
@@ -24,7 +23,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
         invoke-Expression -command ( $MinerCustomConfigCode )
         If ($AbortCurrentPool) {Return}
 
-        $Arguments = "-a $AlgoNorm -o stratum+tcp://$($Pool.Host):$($Pool.Port) -u $($Pool.User) -p $($Password) --api-port $($port) --donate-level 1"
+        $Arguments = "-a $AlgoNorm -t $($ThreadCount) -o stratum+tcp://$($Pools.$Algo.Host):$($Pools.$Algo.Port) -u $($Pools.$Algo.User) -p $($Pools.$Algo.Pass)$($Commands.$_) --keepalive --http-port=$($Variables.CPUMinerAPITCPPort) --donate-level 0"
 
         [PSCustomObject]@{
             Type = "CPU"
@@ -35,7 +34,9 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
             Port = $Port
             Wrap = $false
             URI = $Uri    
-            User = $Pool.User
+            User      = $Pool.User
+            Host      = $Pool.Host
+            Coin      = $Pool.Coin
         }
     }
 }
