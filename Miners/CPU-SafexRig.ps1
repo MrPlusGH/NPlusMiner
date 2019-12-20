8f87d4b8ff0839eead030528bc11be0c1c6b53bb
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2019-12-20 13:17:12 +0100

    Add RandomSFX Support

---
+++ Miners/CPU-SafexRig.ps1
@@ -0,0 +1,43 @@
+if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1;RegisterLoaded(".\Includes\include.ps1")}
+
+$Path = ".\Bin\CPU-SafexRig\safex-rig.exe"
+$Uri = "https://github.com/MrPlusGH/NPlusMiner-MinersBinaries/raw/master/MinersBinaries/CPU-SafexRig/CPU-SafexRig.zip"
+
+$Commands = [PSCustomObject]@{
+    "randomsfx"               = " -a rx/sfx --nicehash" #RandomX
+}
+ 
+$ThreadCount = $ThreadCount = $Variables.ProcessorCount - 2
+
+$Port = $Variables.CPUMinerAPITCPPort #2222
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty Name | % { 
+    $Algo =$_
+	$AlgoNorm = Get-Algorithm($_)
+
+    $Pools.($AlgoNorm) | foreach {
+        $Pool = $_
+
+	If ($_.Coin -eq "TUBE") {$Commands.$Algo = " -a cn-heavy/tube --nicehash"}
+
+        invoke-Expression -command ( $MinerCustomConfigCode )
+        If ($AbortCurrentPool) {Return}
+
+        $Arguments = "-a $AlgoNorm -t $($ThreadCount) -o stratum+tcp://$($Pool.Host):$($Pool.Port) -u $($Pool.User) -p $($Pool.Pass)$($Commands.$_) --keepalive --http-port=$($Variables.CPUMinerAPITCPPort) --donate-level 1"
+
+        [PSCustomObject]@{
+            Type = "CPU"
+            Path = $Path
+            Arguments = Merge-Command -Slave $Arguments -Master $CustomCmdAdds -Type "Command"
+            HashRates = [PSCustomObject]@{($AlgoNorm) = $Stats."$($Name)_$($AlgoNorm)_HashRate".Week * .99} # substract 1% devfee
+            API = "XMRig"
+            Port = $Port
+            Wrap = $false
+            URI = $Uri    
+            User      = $Pool.User
+            Host      = $Pool.Host
+            Coin      = $Pool.Coin
+        }
+    }
+}
