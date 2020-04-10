d3f351cf3e4af94432a11613295bb68cf3188c41
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2020-04-10 19:04:18 +0200

    Create Cpuminer-Arctichash.ps1

---
+++ Miners/Cpuminer-Arctichash.ps1
@@ -0,0 +1,42 @@
+if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1;RegisterLoaded(".\Includes\include.ps1")}
+ 
+$Path = ".\Bin\CPU-Arctichash\cpuminer.exe"
+$Uri = "https://github.com/MrPlusGH/NPlusMiner-MinersBinaries/raw/master/MinersBinaries/CPU-Arctichash/CPU-Arctichash.zip"
+
+$Commands = [PSCustomObject]@{
+    "arctichash" = "-a arctichash" #power2b
+}
+
+$Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+
+    switch ($_) {
+        "hodl" {$ThreadCount = $Variables.ProcessorCount}
+        default {$ThreadCount = $Variables.ProcessorCount - 2}
+    }
+
+	$Algo =$_
+	$AlgoNorm = Get-Algorithm($_)
+
+    $Pools.($AlgoNorm) | foreach {
+        $Pool = $_
+        invoke-Expression -command ( $MinerCustomConfigCode )
+        If ($AbortCurrentPool) {Return}
+        $Arguments = "--cpu-affinity AAAA -q -t $($ThreadCount) -b $($Variables.CPUMinerAPITCPPort) -a $AlgoNorm -o $($Pool.Protocol)://$($Pool.Host):$($Pool.Port) -u $($Pool.User) -p $($Password)"
+
+        [PSCustomObject]@{
+            Type = "CPU"
+            Path = $Path
+            Arguments = Merge-Command -Slave $Arguments -Master $CustomCmdAdds -Type "Command"
+            HashRates = [PSCustomObject]@{($AlgoNorm) = $Stats."$($Name)_$($AlgoNorm)_HashRate".Week}
+            API = "Ccminer"
+            Port = $Variables.CPUMinerAPITCPPort
+            Wrap = $false
+            URI = $Uri
+            User = $Pool.User
+            Host = $Pool.Host
+            Coin = $Pool.Coin
+        }
+    }
+}
