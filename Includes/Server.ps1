aafbcf6b77a9c4ef7d5d555a78fb36bd70ae842f
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2020-05-15 09:49:52 +0200

    Web - Add switching log

--- Includes/Server.ps1
+++ Includes/Server.ps1
@@ -218,7 +218,7 @@ Function Start-Server {
                         &nbsp&nbsp&nbsp $(If($Variables.Rates.($Config.Currency) -gt 0){"$($Config.Passwordcurrency)/$($Config.Currency) $($Variables.Rates.($Config.Currency).ToString("N2"))"})
                         </header>
                         <hr>
-                        <a href="./RunningMiners">Running Miners</a>&nbsp&nbsp&nbsp&nbsp&nbsp<a href="./Benchmarks">Benchmarks</a>
+                        <a href="./RunningMiners">Running Miners</a>&nbsp&nbsp&nbsp&nbsp&nbsp<a href="./Benchmarks">Benchmarks</a>&nbsp&nbsp&nbsp&nbsp&nbsp<a href="./SwitchingLog">Switching log</a>
 "@
 
                     If ($Variables.Paused) {
