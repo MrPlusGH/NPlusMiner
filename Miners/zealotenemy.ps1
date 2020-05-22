2232986e2080c72c6bc75aeaade5696c369d0435
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2020-05-22 16:29:21 +0200

    Update zealotenemy.ps1

--- Miners/zealotenemy.ps1
+++ Miners/zealotenemy.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1")}
 
 $Path = ".\Bin\NVIDIA-zealotenemy\z-enemy.exe"
-$Uri = "https://github.com/Minerx117/miner-binaries/releases/download/2.4/z-enemy-2.4-win-cuda10.0.zip"
+$Uri = "https://github.com/zealot-rvn/z-enemy/releases/download/kawpow262/z-enemy-2.6.2-win-cuda10.1.zip"
 
 $Commands = [PSCustomObject]@{
     "aeriumx" = " -i 23" #AeriumX(fastest)
@@ -20,23 +20,35 @@ $Commands = [PSCustomObject]@{
     "xevan"   = " -i 22" #Xevan (fastest)
     #"x17"        = "" #X17(trex faster)
     "hex" = " -i 24" #Hex (fastest)
+    # "kawpow" = "" #Kawpow
 }
 
+$Port = $Variables.NVIDIAMinerAPITCPPort
 $Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+$WinningCustomConfig = [PSCustomObject]@{}
 
 $Commands.PSObject.Properties.Name | ForEach-Object {
-    $Algo = Get-Algorithm($_)
-    [PSCustomObject]@{
-        Type      = "NVIDIA"
-        Path      = $Path
-        Arguments = "-b $($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -R 1 -q -a $_ -o stratum+tcp://$($Pools.($Algo).Host):$($Pools.($Algo).Port) -u $($Pools.($Algo).User) -p $($Pools.($Algo).Pass)$($Commands.$_)"
-        HashRates = [PSCustomObject]@{($Algo) = $Stats."$($Name)_$($Algo)_HashRate".Day * .99} # substract 1% devfee
-        API       = "ccminer"
-        Port      = $Variables.NVIDIAMinerAPITCPPort
-        Wrap      = $false
-        URI       = $Uri
-        User      = $Pool.($Algo).User
-        Host = $Pool.($Algo).Host
-        Coin = $Pool.($Algo).Coin
+    $Algo =$_
+    $AlgoNorm = Get-Algorithm($_)
+    $Pools.($AlgoNorm) | foreach {
+        $Pool = $_
+        invoke-Expression -command ( $MinerCustomConfigCode )
+        If ($AbortCurrentPool) {Return}
+
+        $Arguments = " -b $($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -R 1 -q -a $Algo -o stratum+tcp://$($Pool.Host):$($Pool.Port) -u $($Pool.User) -p $($Password)"
+        
+        [PSCustomObject]@{
+            Type      = "NVIDIA"
+            Path      = $Path
+            Arguments = Merge-Command -Slave $Arguments -Master $CustomCmdAdds -Type "Command"
+            HashRates = [PSCustomObject]@{($AlgoNorm) = $Stats."$($Name)_$($AlgoNorm)_HashRate".Day * .99} # substract 1% devfee
+            API       = "ccminer"
+            Port      = $Variables.NVIDIAMinerAPITCPPort
+            Wrap      = $false
+            URI       = $Uri
+            User      = $Pool.User
+            Host = $Pool.Host
+            Coin = $Pool.Coin
+        }
     }
 }
