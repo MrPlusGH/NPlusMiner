2522d69c0934ed1d909968b24106b7952e2d0ba6
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2020-05-24 13:52:10 +0200

    t-rex 0.15.7

--- Miners/trex.ps1
+++ Miners/trex.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1")}
 
 $Path = ".\Bin\NVIDIA-trex\t-rex.exe"
-$Uri = "https://github.com/trexminer/T-Rex/releases/download/0.15.6/t-rex-0.15.6-win-cuda10.0.zip"
+$Uri = "https://github.com/trexminer/T-Rex/releases/download/0.15.7/t-rex-0.15.7-win-cuda10.0.zip"
  
 $Commands = [PSCustomObject]@{
     "astralhash" = "" #Astralhash
