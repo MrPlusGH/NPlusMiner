62b19a839ecdc01311fb3d4bd75d701bc2cf8ca6
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2020-06-10 09:51:46 +0200

    --nicehash

--- Miners/CPU-XMRig.ps1
+++ Miners/CPU-XMRig.ps1
@@ -6,18 +6,18 @@ $Uri = "https://github.com/xmrig/xmrig/releases/download/v5.11.0/xmrig-5.11.0-ms
 $Commands = [PSCustomObject]@{
     # "cryptonightr"        = " -a cryptonight/r --nicehash" #cryptonight/r
     # "cryptonight-monero"  = " -a cryptonight/r" #cryptonight/r
-    "randomxmonero"         = " -a rx/0 --nicehash" #RandomX
-    "randomx"               = " -a rx/0 --nicehash" #RandomX
-    "randomsfx"             = " -a rx/sfx --nicehash" #RandomX
-    "randomarq"             = " -a rx/arq --nicehash" #Randomarq
-    "cryptonightv7"         = " -a cn/1 --nicehash" #cryptonightv7
-    # "cryptonight_gpu"       = " -a cn/gpu --nicehash" #cryptonightGPU
-    "cryptonight_xeq"       = " -a cn/gpu --nicehash" #cryptonightGPU
-    "cryptonight_heavy"     = " -a cn-heavy/0 --nicehash" #cryptonight_heavyx
-    "cryptonight_heavyx"    = " -a cn/double --nicehash" #cryptonight_heavyx
-    "cryptonight_saber"     = " -a cn-heavy/0 --nicehash" #cryptonightGPU
-    "cryptonight_fast"      = " -a cn/half --nicehash" #cryptonightFast
-    "cryptonight_haven"      = " -a cn-heavy/xhv --nicehash" #cryptonightFast
+    "randomxmonero"         = " -a rx/0" #RandomX
+    "randomx"               = " -a rx/0" #RandomX
+    "randomsfx"             = " -a rx/sfx" #RandomX
+    "randomarq"             = " -a rx/arq" #Randomarq
+    "cryptonightv7"         = " -a cn/1" #cryptonightv7
+    # "cryptonight_gpu"       = " -a cn/gpu" #cryptonightGPU
+    "cryptonight_xeq"       = " -a cn/gpu" #cryptonightGPU
+    "cryptonight_heavy"     = " -a cn-heavy/0" #cryptonight_heavyx
+    "cryptonight_heavyx"    = " -a cn/double" #cryptonight_heavyx
+    "cryptonight_saber"     = " -a cn-heavy/0" #cryptonightGPU
+    "cryptonight_fast"      = " -a cn/half" #cryptonightFast
+    "cryptonight_haven"      = " -a cn-heavy/xhv" #cryptonightFast
 }
  
 $ThreadCount = $ThreadCount = $Variables.ProcessorCount - 2
@@ -32,7 +32,8 @@ $Commands.PSObject.Properties.Name | % {
     $Pools.($AlgoNorm) | foreach {
         $Pool = $_
 
-    If ($_.Coin -eq "TUBE") {$Commands.$Algo = " -a cn-heavy/tube --nicehash"}
+        If ($_.Coin -eq "TUBE") {$Commands.$Algo = " -a cn-heavy/tube"}
+        If ($Pool.Host -like "*nicehash*") {$Commands.$Algo += " --nicehash"}
 
         invoke-Expression -command ( $MinerCustomConfigCode )
         If ($AbortCurrentPool) {Return}
