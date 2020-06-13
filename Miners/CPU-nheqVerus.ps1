05c90006b981a311315168481f6d61c3aa59e952
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2020-06-13 19:39:19 +0200

    Update CPU-nheqVerus.ps1

--- Miners/CPU-nheqVerus.ps1
+++ Miners/CPU-nheqVerus.ps1
@@ -1,7 +1,7 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1;RegisterLoaded(".\Includes\include.ps1")}
 
 $Path = ".\Bin\CPU-nheqVerus\nheqminer.exe"
-$Uri = "https://github.com/VerusCoin/nheqminer/releases/download/v0.8.1/nheqminer-Windows-v0.8.1.zip"
+$Uri = "https://github.com/MrPlusGH/NPlusMiner-MinersBinaries/raw/master/MinersBinaries/CPU-nheqVerus/nheqminer-Windows-v0.8.1.zip"
 
 $Commands = [PSCustomObject]@{
     "verus" = "" #Verushash
