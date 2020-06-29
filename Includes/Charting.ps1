436ae5841a2ac82f67529aa461e279f04ac305e9
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2020-06-29 12:39:16 +0200

    Update Charting.ps1

--- Includes/Charting.ps1
+++ Includes/Charting.ps1
@@ -103,7 +103,7 @@ $EndColor       = If ($Config.ChartGradientMaxColor) {$Config.ChartGradientMaxCo
 
 Switch ($Chart) {
     "Front7DaysEarnings" {
-           $datasource = If (Test-Path ".\logs\DailyEarnings.csv" ) {Import-Csv ".\logs\DailyEarnings.csv" | ? {[DateTime]$_.date -ge (Get-Date).AddDays(-14)}}
+           $datasource = If (Test-Path ".\logs\DailyEarnings.csv" ) {Import-Csv ".\logs\DailyEarnings.csv" | ? {[DateTime]$_.date -ge (Get-Date).AddDays(-7)}}
            $datasource = $datasource | select *,@{Name="DaySum";Expression={$Date = $_.date;(($datasource | ? {$_.date -eq $Date}).DailyEarnings | measure -sum).sum }}
          
            $chart1 = New-object System.Windows.Forms.DataVisualization.Charting.Chart
