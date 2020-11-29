14b619a413d85a205f32e491661b7019db4f4ed8
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2020-11-29 15:12:34 +0100

    Add Curvehash Support

--- Miners/Cpu-rplant.ps1
+++ Miners/Cpu-rplant.ps1
@@ -1,11 +1,12 @@
 if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1;RegisterLoaded(".\Includes\include.ps1")}
 
 $Path = ".\Bin\CPU-rplant\cpuminer-sse42.exe"
-$Uri = "https://github.com/MrPlusGH/NPlusMiner-MinersBinaries/raw/master/MinersBinaries/Cpu-rplant/cpuminer-opt-win.zip"
+$Uri = "https://github.com/rplant8/cpuminer-opt-rplant/releases/download/4.5.18/cpuminer-opt-win.zip"
 
 $Commands = [PSCustomObject]@{
     "yescryptR8G" = "" #YescryptR8
     "yespowerIOTS" = "" #yespowerIOTS
+    "curve" = "" #curvehash
 }
 
 $Name = Get-Item $MyInvocation.MyCommand.Path | Select-Object -ExpandProperty BaseName
