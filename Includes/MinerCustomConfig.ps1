dec76304296169f18095f32ea1ca9f2e6f05848e
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2021-02-04 11:18:46 +0100

    Copyright Update

--- Includes/MinerCustomConfig.ps1
+++ Includes/MinerCustomConfig.ps1
@@ -1,6 +1,6 @@
 <#
 This file is part of NPlusMiner
-Copyright (c) 2018-2019 MrPlus
+Copyright (c) 2018-2021 MrPlus
 
 NPlusMiner is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
