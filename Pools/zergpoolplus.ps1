209fa87882ad333917d8ee723bfdd61570cd851c
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2021-02-10 14:58:56 +0100

    Fix asia stratum

--- Pools/zergpoolplus.ps1
+++ Pools/zergpoolplus.ps1
@@ -39,15 +39,14 @@ $dtAlgos | foreach {
     $PoolPassword = If ( ! $Config.PartyWhenAvailable ) {"$($WorkerName),c=$($PwdCurr)"} else { "$($WorkerName),c=$($PwdCurr),m=party.NPlusMiner" }
     $PoolPassword = If ( $Pool.symbol) { "$($PoolPassword),mc=$($Pool.symbol)" } else { $PoolPassword }
 
-    $Locations = "eu", "na", "sea", "jp"
+    $Locations = "eu", "na", "asia"
     $Locations | ForEach-Object {
         $Pool_Location = $_
         
         switch ($Pool_Location) {
             "eu"    {$Location = "EU"}
             "na"    {$Location = "US"}
-            "sea"   {$Location = "JP"}
-            "jp"   {$Location = "JP"}
+            "asia"   {$Location = "JP"}
             default {$Location = "US"}
         }
         $PoolHost = "$($Pool.algo).$($Pool_Location)$($HostSuffix)"
