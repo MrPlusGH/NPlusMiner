021a7478f649d5616f5148c72aeb5076909ab855
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2021-03-24 09:24:35 +0100

    SRBMiner-Multi-0-7-1

--- Miners/SRBMiner.ps1
+++ Miners/SRBMiner.ps1
@@ -1,7 +1,7 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
 $Path = ".\Bin\cpu-SRBMiner\SRBMiner-MULTI.exe"
-$Uri = "https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.0/SRBMiner-Multi-0-7-0-win64.zip"
+$Uri = "https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.1/SRBMiner-Multi-0-7-1-win64.zip"
 
 $Commands = [PSCustomObject]@{ 
     # "randomx"            = " --randomx-use-1gb-pages" #randomx 
