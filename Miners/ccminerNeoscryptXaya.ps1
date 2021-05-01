531c436ed23bce19b86854420571af1d6919587e
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2021-05-01 09:47:28 +0200

    Fix GPU selection for Xaya

--- Miners/ccminerNeoscryptXaya.ps1
+++ Miners/ccminerNeoscryptXaya.ps1
@@ -18,7 +18,7 @@ $Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | 
         invoke-Expression -command ( $MinerCustomConfigCode )
         If ($AbortCurrentPool) {Return}
 
-        $Arguments = "--cpu-priority 5 -b $($Variables.NVIDIAMinerAPITCPPort) -N 1 -R 1 -o stratum+tcp://$($Pool.Host):$($Pool.Port) -u $($Pool.User) -p $($Password)"
+        $Arguments = "--cpu-priority 5 -b $($Variables.NVIDIAMinerAPITCPPort) -d $($Config.SelGPUCC) -N 1 -R 1 -o stratum+tcp://$($Pool.Host):$($Pool.Port) -u $($Pool.User) -p $($Password)"
 
         [PSCustomObject]@{
             Type = "NVIDIA"
