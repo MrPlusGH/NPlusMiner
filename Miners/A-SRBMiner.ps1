6d78708c9002d9e9a3aab0c5997f78475ee0ba76
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2021-08-03 18:53:55 +0200

    Add Heavyhash support

--- Miners/A-SRBMiner.ps1
+++ Miners/A-SRBMiner.ps1
@@ -1,6 +1,6 @@
 If (-not (IsLoaded(".\Includes\include.ps1"))) { . .\Includes\include.ps1; RegisterLoaded(".\Includes\include.ps1") }
 
-$Path = ".\Bin\cpu-SRBMiner\SRBMiner-MULTI.exe"
+$Path = ".\Bin\AMD-SRBMiner\SRBMiner-MULTI.exe"
 # $Uri = "https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.1/SRBMiner-Multi-0-7-1-win64.zip"
 # $Uri = "https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.3/SRBMiner-Multi-0-7-3-win64.zip"
 $Uri = "https://github.com/doktor83/SRBMiner-Multi/releases/download/0.7.9/SRBMiner-Multi-0-7-9-win64.zip"
@@ -11,19 +11,19 @@ $Commands = [PSCustomObject]@{
     # "randomarq"          = " --randomx-use-1gb-pages" #randomarq  
     # "randomsfx"          = " --randomx-use-1gb-pages" #randomsfx  
     # "eaglesong"          = "" #eaglesong  
-    "autolykos"           = "" #autolykos    
-    "yescrypt"           = "" #yescrypt    
-    "curvehash"           = "" #yescrypt    
+    # "autolykos"           = "" #autolykos    
+    # "yescrypt"           = "" #yescrypt    
+    # "curvehash"           = "" #yescrypt    
     # "yescryptR16"        = "" #yescryptR16  
     # "yescryptR32"        = "" #yescryptR32   
-    "yespower"           = "" #yespower 
-    "yespowerSUGAR"           = "" #yespowerSUGAR
+    # "yespower"           = "" #yespower 
+    # "yespowerSUGAR"           = "" #yespowerSUGAR
     # "yespowerr16"        = "" #yespowerr16 
     # "cryptonight-monero" = " --randomx-use-1gb-pages" #randomx
     "phi5" = "" #phi5
     # "verthash" = "" #verthash
-    "rx2" = "" #verthash
-    "panthera" = "" #panthera
+    # "rx2" = "" #verthash
+    # "panthera" = "" #panthera
     "heavyhash" = "" #heavyhash
 }
 
@@ -45,10 +45,10 @@ $Commands | Get-Member -MemberType NoteProperty | Select-Object -ExpandProperty 
         #Curve diff doesn't play well on ZPool
         If ($Pool.Host -like "*zpool*" -and $AlgoNorm -eq "curvehash") {Return}
 
-        $Arguments = "--algorithm $($AlgoNorm) --pool stratum+tcp://$($Pool.Host):$($Pool.Port) --cpu-threads $($ThreadCount) --nicehash true --send-stales true --api-enable --api-port $($Variables.CPUMinerAPITCPPort) --disable-gpu --wallet $($Pool.User) --password $($Password)"
+        $Arguments = "--algorithm $($AlgoNorm) --pool stratum+tcp://$($Pool.Host):$($Pool.Port) --cpu-threads $($ThreadCount) --nicehash true --send-stales true --api-enable --api-port $($Variables.CPUMinerAPITCPPort) --disable-cpu --wallet $($Pool.User) --password $($Password)"
 
         [PSCustomObject]@{
-            Type = "CPU"
+            Type = "AMD"
             Path = $Path
             Arguments = Merge-Command -Slave $Arguments -Master $CustomCmdAdds -Type "Command"
             HashRates = [PSCustomObject]@{($AlgoNorm) = $Stats."$($Name)_$($AlgoNorm)_HashRate".Week * .9915 } # substract 0.85% devfee
---
+++ Miners/N-suprminer.ps1
@@ -0,0 +1,37 @@
+if (!(IsLoaded(".\Includes\include.ps1"))) {. .\Includes\include.ps1;RegisterLoaded(".\Includes\include.ps1")}
+
+$Path = ".\Bin\NVIDIA-suprminer\suprminer.exe"
+$Uri = "https://github.com/ocminer/suprminer-releases/raw/main/suprminer-winx86_64_cuda11_1.zip"
+
+$Commands = [PSCustomObject]@{
+    "heavyhash" = "" #heavyhash
+}
+
+$Name = (Get-Item $script:MyInvocation.MyCommand.Path).BaseName
+
+$Commands | Get-Member -MemberType NoteProperty | Select -ExpandProperty Name | ForEach {
+    $Algo =$_
+    $AlgoNorm = Get-Algorithm($_)
+
+    $Pools.($AlgoNorm) | foreach {
+        $Pool = $_
+        invoke-Expression -command ( $MinerCustomConfigCode )
+        If ($AbortCurrentPool) {Return}
+
+        $Arguments = "-b $($Variables.NVIDIAMinerAPITCPPort) -R 1 -a OBTC -o stratum+tcp://$($Pool.Host):$($Pool.Port) -u $($Pool.User) -p $($Password)"
+
+        [PSCustomObject]@{
+            Type = "NVIDIA"
+            Path = $Path
+            Arguments = Merge-Command -Slave $Arguments -Master $CustomCmdAdds -Type "Command"
+            HashRates = [PSCustomObject]@{($AlgoNorm) = $Stats."$($Name)_$($AlgoNorm)_HashRate".Week}
+            API = "ccminer"
+            Port = $Variables.NVIDIAMinerAPITCPPort
+            Wrap = $false
+            URI = $Uri
+            User = $Pool.User
+            Host = $Pool.Host
+            Coin = $Pool.Coin
+        }
+    }
+}
