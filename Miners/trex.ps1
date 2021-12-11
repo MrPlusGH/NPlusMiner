edee1a440a063892b9a49e652717fbaef0866a46
MrPlusGH <36303784+MrPlusGH@users.noreply.github.com>
2021-12-11 12:25:33 +0100

    TRex 0.24.8

--- Miners/trex.ps1
+++ Miners/trex.ps1
@@ -5,7 +5,8 @@ $Path = ".\Bin\NVIDIA-trex\t-rex.exe"
 # $Uri = "https://github.com/trexminer/T-Rex/releases/download/0.21.6/t-rex-0.21.6-win.zip"
 # $Uri = "https://github.com/trexminer/T-Rex/releases/download/0.22.1/t-rex-0.22.1-win.zip"
 # $Uri = "https://github.com/trexminer/T-Rex/releases/download/0.24.5/t-rex-0.24.5-win.zip"
-$Uri = "https://github.com/trexminer/T-Rex/releases/download/0.24.7/t-rex-0.24.7-win.zip"
+# $Uri = "https://github.com/trexminer/T-Rex/releases/download/0.24.7/t-rex-0.24.7-win.zip"
+$Uri = "https://github.com/trexminer/T-Rex/releases/download/0.24.8/t-rex-0.24.8-win.zip"
 
 $Commands = [PSCustomObject]@{
     "kawpow"          = "" #kawpow 
